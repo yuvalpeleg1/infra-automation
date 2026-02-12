b84bd981e56200f951f4b391f83cbc21eac2ab07
Yuval <yuvalpelegbox@gmail.com>
2026-02-12 21:04:54 +0200

    Validate on real EC2 AWS instances types with ec2_instances.json

--- src/validation.py
+++ src/validation.py
@@ -1,24 +1,41 @@
 from typing import Annotated
 from pydantic import BaseModel, Field, field_validator
+import json
+
+
+def load_ec2_types():
+    try:
+        with open("../configs/ec2_instances.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        raise RuntimeError("EC2 instances json not found")
+
+
+EC2_TYPES = load_ec2_types()
 
 
 class VMConfig(BaseModel):
     name: Annotated[str, Field(..., min_length=1)]
     os: str
-    cpu: Annotated[str, Field(..., gt=0)]
-    ram: Annotated[str, Field(..., gt=0)]
+    instance_type: str
 
     @field_validator("os")
-    def check_os(cls, os):
-        os = os.lower()
-        if os not in [
+    def validate_os(cls, os):
+        available_os = [
             "amazon linux",
             "windows",
             "red hat enterprise linux",
             "ubuntu",
             "ubuntu pro",
-        ]:
-            raise ValueError(
-                "Os must be amazon linux | windows | red hat enterprise linux | ubuntu | ubuntu pro"
-            )
+        ]
+        os = os.lower()
+        if os not in available_os:
+            raise ValueError(f"Os must be one of: {available_os}")
         return os
+
+    @field_validator("instance_type")
+    def validate_instance_type(cls, instance_type):
+        if instance_type not in EC2_TYPES:
+            raise ValueError(
+                f"Invalid instance type. These are the options: {list(EC2_TYPES.keys())}"
+            )
